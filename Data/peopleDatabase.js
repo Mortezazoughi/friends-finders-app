e802ebe70888d5ca0e8367de05e3a99488e57a71
Mortezazoughi <47608924+Mortezazoughi@users.noreply.github.com>
2019-05-28 15:22:33 -0400

    WIP: Adding the html pages.

---
+++ Data/peopleDatabase.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Olivia',
+		photo: 'https://www.billboard.com/files/styles/article_main_image/public/media/olivia-newton-john-you-have-to-believe-vid-2015-billboard-650.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sarah',
+		photo: 'https://pmctvline2.files.wordpress.com/2015/08/sarah-shahi-person-of-interest-season-5-returning.jpg?w=620',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Behrouz',
+		photo: 'https://i.pinimg.com/originals/18/5c/51/185c51f2f4327f55dce2b23df92839cf.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Ali',
+		photo: 'https://www.onthisday.com/images/people/muhammad-ali-medium.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Cloe',
+		photo: 'https://www.cheatsheet.com/wp-content/uploads/2018/02/Chloe-Kim-640x372.png',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Tom',
+		photo: 'https://pixel.nymag.com/imgs/daily/vulture/2017/10/02/02-tom-petty-obit-2.w700.h700.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kathrine',
+		photo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/katherine-schwarzenegger-attends-the-mandy-moore-x-fossil-news-photo-1052651564-1558039709.jpg?crop=1.00xw:0.663xh;0,0.0315xh&resize=480:*',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Donna',
+		photo: 'https://s22313.pcdn.co/wp-content/uploads/DonnaSummer_RockHall.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Barry',
+		photo: 'https://i.pinimg.com/originals/29/30/23/2930230334d2941b8f91bb003fda457f.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Roger',
+		photo: 'https://nyppagesix.files.wordpress.com/2017/05/673576878.jpg?quality=90&strip=all&w=618&h=410&crop=1',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
