154eda46faef49b21200e80d6fb8e3c0ac9aeb18
Mort <mortezazoughi@gmail.com>
2019-05-30 18:28:28 -0400

    wip

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -3,16 +3,16 @@ var path = require('path');
 
 // Export HTML routes
 module.exports = function(app) {
-	// console.log('___ENTER htmlRoutes.js___');
+  // console.log('___ENTER htmlRoutes.js___');
 
-	// Home page
-	app.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/home.html'));
-	});
+  // Home page
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
 
-	// Survey page
-	app.get('/survey', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/survey.html'));
-        console.log(__dirname);
-	});
+  // Survey page
+  app.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
+    console.log(__dirname);
+  });
 };
