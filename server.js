1c2142c003799fbad6ce9019f369102969f81b95
Mort <mortezazoughi@gmail.com>
2019-05-29 20:20:34 -0400

    WIP: updated server

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // Add the application routes
 require(path.join(__dirname, './routes/apiRoutes'))(app);
